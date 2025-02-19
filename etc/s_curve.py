76da0de53862b49b4b35169f325827bf59ffa600
masahito3 <121845135+masahito3@users.noreply.github.com>
2025-02-19 21:28:46 +0900

    s_curve diagram

--- etc/s_curve.py
+++ etc/s_curve.py
@@ -2,9 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import *
 
-def s(t):
-    return 3.0*t**2-2*t**3
-
+def s(r):
+    return 6*r**5-15*r**4+10*r**3
 
 fig=plt.figure(figsize=(4,4),layout="constrained") # inch
 ax=fig.add_subplot()
@@ -13,13 +12,18 @@ ax.set_xlabel("r")
 
 x=np.linspace(0,1,100)
 y=[s(x) for x in x]
-ax.plot(x,y,color="blue")
+ax.plot(x,y,color="blue",clip_on=False)
 y=[1-s(x) for x in x]
-ax.plot(x,y,color="red")
+ax.plot(x,y,color="red",clip_on=False)
 
 
 ax.grid("on")
 ax.set(ylim=(0,1),xlim=(0,1))
+
+ax.text(0.35,0.7,r"$1-s(r)$",va="center",ha="right",fontsize=20,color="red")
+ax.text(0.7,0.7,r"$s(r)$",va="center",ha="left",fontsize=20,color="blue")
+
+
 #ax.xaxis.set_major_locator(MultipleLocator(base=1.0))
 #ax.yaxis.set_major_locator(MultipleLocator(base=1.0))
 plt.savefig("s_curve.svg")
